4f23f0f35f6335d3ecc222f861a4cf056973b73b
Wilfred Wahee Jr <wilfredwaheejr@gmail.com>
2023-04-18 02:25:07 -0400

    Album info panel

--- metacritic.js
+++ metacritic.js
@@ -10,6 +10,44 @@ function init() {
           .text(album)
           .property("value", album);
       });
-  })}
+
+       // Use the first sample from the list to build the initial plots
+    var firstAlbum = albumNames[0];
+    buildCharts(firstAlbum);
+    buildAlbumInfo(firstAlbum);
+  });
+}
   
   init();
+
+  function optionChanged(newAlbum) {
+    // Fetch new data each time a new sample is selected
+    buildAlbumInfo(newAlbum);
+    buildCharts(newAlbum);
+    
+  }
+  
+  // Album Info Panel 
+  function buildAlbumInfo(album) {
+    d3.json("adjusted_music_scores.json").then(function(data){
+        var albuminfo = data.map(person =>
+            person.summary).sort((a,b) => b - a);
+      // Filter the data for the object with the desired sample number
+      var resultArray = albuminfo.filter(sampleObj => sampleObj.title == album);
+      var result = resultArray[0];
+      // Use d3 to select the panel with id of `#sample-metadata`
+      var PANEL = d3.select("#album-info");
+  
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
+  
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+  
+    });
+  }
+  
